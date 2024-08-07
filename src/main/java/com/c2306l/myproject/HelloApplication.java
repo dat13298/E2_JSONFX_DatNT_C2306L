03dca6906d519cad5a12ecfcb60572da0fb3fa6d
dat13298 <157354116+dat13298@users.noreply.github.com>
2024-08-08 02:11:42 +0700

    E3

--- src/main/java/com/c2306l/myproject/HelloApplication.java
+++ src/main/java/com/c2306l/myproject/HelloApplication.java
@@ -5,11 +5,12 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
 
 public class HelloApplication extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/com/c2306l/myproject/Login.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Login.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Login Form");
         stage.setScene(scene);
